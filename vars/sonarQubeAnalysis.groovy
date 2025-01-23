cad0b9b9dd78a7e9c8592eca0bb732d221a60eed
Chaitramayu <mayu.ct2023@gmail.com>
2025-01-23 07:34:45 -0500

    Create sonarQubeAnalysis.groovy

---
+++ vars/sonarQubeAnalysis.groovy
@@ -0,0 +1,5 @@
+def call(Map config = [:]) {
+    // Default configuration if not provided
+    def repoName = config.repoName ?: 'default-project'  // Set default if no repoName is passed
+    def sonarServer = config.sonarServer ?: 'sonar-server'  // Set default SonarQube server name
+    def scannerHome = config.scannerHome ?: '/usr/local/sonar-scanner'  // Path to SonarQube Scanner (can use $SCANNER_HOME)
