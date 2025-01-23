a192becc6146540c026a14792e17f384f7e441b8
Chaitramayu <mayu.ct2023@gmail.com>
2025-01-23 08:30:53 -0500

    Update sonarQubeAnalysis.groovy

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,5 +1,19 @@
-def call(Map config = [:]) {
-    // Default configuration if not provided
-    def repoName = config.repoName ?: 'default-project'  // Set default if no repoName is passed
-    def sonarServer = config.sonarServer ?: 'sonar-server'  // Set default SonarQube server name
-    def scannerHome = config.scannerHome ?: '/usr/local/sonar-server'  // Path to SonarQube Scanner (can use $SCANNER_HOME)
+// vars/sonarScannerAnalysis.groovy
+
+def call(String sonarProjectKey, String sonarProjectName, String sonarServer = 'sonar-server') {
+    script {
+        
+        
+        // Define the SonarQube scanner home path
+        def sonarScannerHome = tool name: 'sonarqube'
+
+        // Run the SonarQube scanner with the specified configuration
+        withSonarQubeEnv(sonarServer) {
+            sh """
+            ${sonarScannerHome}/bin/sonar-server \
+            -Dsonar.projectKey=${sonarProjectKey} \
+            -Dsonar.projectName=${sonarProjectName} \
+            """
+        }
+    }
+}
